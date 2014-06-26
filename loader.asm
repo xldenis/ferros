a8c98c89319cf7a748f90010f7d20d519b3a76c7
Xavier Denis <xldenis@gmail.com>
2014-06-25 22:59:46 -0400

    Switch to protected mode.

--- loader.asm
+++ loader.asm
@@ -3,21 +3,73 @@
 ; 16 bit ax bx cx dx
 ; 32 bit eax ebx ecx edx
 
-use16                 ; BIOS is in 16-bit real mode
-org 0x7c00            ; bootloader loaded to offset 0x7c00
+use16                      ; BIOS is in 16-bit real mode
+org 0x7c00                 ; bootloader loaded to offset 0x7c00
 
 xor ax, ax
 mov ds, ax
+mov es, ax
 
 mov si, msg
 
 call print
 
-cli                   ; clear interrupts while stack is being setup
+cli                        ; clear interrupts while stack is being setup
 mov ax, 0x9000 
-mov ss, ax            ; start stack at 0x9000
+mov ss, ax                 ; start stack at 0x9000
 mov sp, 0xffff
-sti                   ; let the interrupts resume
+sti                        ; let the interrupts resume
+
+
+; mov ah, 2             ; disk read
+; mov al, 24            ; num blocks
+; mov ch, 0             ; cylinder
+; mov cl, 2             ; sector
+; mov dh, 0             ; disk head
+; mov bx, 0x7e00        ; put kernel at 0x7e00
+; int 0x13              ; reset disk controller
+; jc error 
+
+cli
+lgdt [gdtr]                ; global descriptor table (register)
+lidt [idtr]                ; interrupt descriptor table (register)
+
+mov eax, cr0
+or eax, 1
+mov cr0, eax               ; switch to protected mode
+jmp 0x8:pmode
+
+pmode:
+  use32
+  jmp $
+
+
+gdtr:
+    dw (gdt_end - gdt) + 1 ; size
+    dd gdt                 ; offset
+
+idtr:                      ; lol ignore all errors
+    dw 0
+    dd 0
+
+gdt:
+                           ; null entry
+    dq 0
+                           ; code entry
+    dw 0xffff              ; limit 0:15
+    dw 0x0000              ; base 0:15
+    db 0x00                ; base 16:23
+    db 0b10011010          ; access byte - code
+    db 0x4f                ; flags/(limit 16:19). flag is set to 32 bit protected mode
+    db 0x00                ; base 24:31
+                           ; data entry
+    dw 0xffff              ; limit 0:15
+    dw 0x0000              ; base 0:15
+    db 0x00                ; base 16:23
+    db 0b10010010          ; access byte - data
+    db 0x4f                ; flags/(limit 16:19). flag is set to 32 bit protected mode
+    db 0x00                ; base 24:31
+gdt_end:
 
 print:
   lodsb
@@ -30,8 +82,14 @@ print:
 done:
   ret
 
-msg db 'Hello World', 13, 10, 0
+error:
+  mov si, err
+  call print
+  jmp $
 
-times 510-($-$$) db 0 ; pad with 0s
-db 0x55               ; place 0xAA55 at offset 510
+msg db 'Hello World', 13, 10, 0
+err db 'Could not read disk', 13, 10, 0
+
+times 510-($-$$) db 0      ; pad with 0s
+db 0x55                    ; place 0xAA55 at offset 510
 db 0xaa
