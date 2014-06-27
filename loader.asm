5e7fb231a09fd8cc83cb74f3da5409c8ae08a449
Xavier Denis <xldenis@gmail.com>
2014-06-26 21:37:48 -0400

    Refactor code locations, add disk loading code, fix jump to PMODE.

--- loader.asm
+++ loader.asm
@@ -3,9 +3,32 @@
 ; 16 bit ax bx cx dx
 ; 32 bit eax ebx ecx edx
 
+extern main
+
 use16                      ; BIOS is in 16-bit real mode
 org 0x7c00                 ; bootloader loaded to offset 0x7c00
 
+jmp start
+
+print:
+  lodsb
+  or al, al
+  jz done
+  mov ah, 0eh
+  mov bx, 0007
+  int 0x10
+  jmp print
+done:
+  ret
+
+error:
+  mov si, err
+  call print
+  jmp $
+
+
+
+start: 
 xor ax, ax
 mov ds, ax
 mov es, ax
@@ -21,14 +44,14 @@ mov sp, 0xffff
 sti                        ; let the interrupts resume
 
 
-; mov ah, 2             ; disk read
-; mov al, 24            ; num blocks
-; mov ch, 0             ; cylinder
-; mov cl, 2             ; sector
-; mov dh, 0             ; disk head
-; mov bx, 0x7e00        ; put kernel at 0x7e00
-; int 0x13              ; reset disk controller
-; jc error 
+mov ah, 2             ; disk read
+mov al, 24            ; num blocks
+mov ch, 0             ; cylinder
+mov cl, 2             ; sector
+mov dh, 0             ; disk head
+mov bx, 0x7e00        ; put kernel at 0x7e00
+int 0x13              ; reset disk controller
+jc error 
 
 cli
 lgdt [gdtr]                ; global descriptor table (register)
@@ -36,13 +59,29 @@ lidt [idtr]                ; interrupt descriptor table (register)
 
 mov eax, cr0
 or eax, 1
-mov cr0, eax               ; switch to protected mode
-jmp 0x8:pmode
+mov cr0, eax            ; switch to protected mode
+
+jmp 0x08:pmode
 
 pmode:
-  use32
-  jmp $
+use32
 
+; load all the other segments with 32-bit segment 2 (data)
+mov eax, 2 << 3
+mov ds, eax
+mov es, eax
+mov fs, eax
+mov gs, eax
+mov ss, eax
+; set up aligned stack bottom
+mov esp, 0x7c00
+; enable SSE instructions
+mov eax, cr4
+or eax, 512
+mov cr4, eax
+mov dword[gs:0x30], 0
+
+call main
 
 gdtr:
     dw (gdt_end - gdt) + 1 ; size
@@ -70,22 +109,6 @@ gdt:
     db 0x4f                ; flags/(limit 16:19). flag is set to 32 bit protected mode
     db 0x00                ; base 24:31
 gdt_end:
-
-print:
-  lodsb
-  or al, al
-  jz done
-  mov ah, 0eh
-  mov bx, 0007
-  int 0x10
-  jmp print
-done:
-  ret
-
-error:
-  mov si, err
-  call print
-  jmp $
 
 msg db 'Hello World', 13, 10, 0
 err db 'Could not read disk', 13, 10, 0
