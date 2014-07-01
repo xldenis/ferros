38d2fd3abe97035ebbdab5c592fd04a41d009725
Xavier Denis <xldenis@gmail.com>
2014-07-01 12:36:31 -0400

    add 32 bit printing function

--- loader.asm
+++ loader.asm
@@ -10,23 +10,35 @@ org 0x7c00                 ; bootloader loaded to offset 0x7c00
 
 jmp start
 
-print:
-  lodsb
-  or al, al
-  jz done
-  mov ah, 0eh
-  mov bx, 0007
-  int 0x10
-  jmp print
-done:
-  ret
-
-error:
-  mov si, err
-  call print
-  jmp $
-
-
+dochar:
+    call cprint              ; print one character
+sprint:
+    mov eax, [esi]          ; string char to AL
+    lea esi, [esi+1]
+    cmp al, 0
+    jne dochar               ; else, we're done
+    add byte [ypos], 1       ; down one row
+    mov byte [xpos], 0       ; back to left
+    ret
+ 
+cprint:
+    mov ah, 0x0F             ; attrib = white on black
+    mov ecx, eax             ; save char/attribute
+    movzx eax, byte [ypos]
+    mov edx, 160             ; 2 bytes (char/attrib)
+    mul edx                  ; for 80 columns
+    movzx ebx, byte [xpos]
+    shl ebx, 1               ; times 2 to skip attrib
+ 
+    mov edi, 0xb8000         ; start of video memory
+    add edi, eax             ; add y offset
+    add edi, ebx             ; add x offset
+ 
+    mov eax, ecx             ; restore char/attribute
+    mov word [ds:edi], ax
+    add byte [xpos], 1       ; advance to right
+ 
+    ret
 
 start: 
 xor ax, ax
@@ -34,8 +46,6 @@ mov ds, ax
 mov es, ax
 
 mov si, msg
-
-call print
 
 cli                        ; clear interrupts while stack is being setup
 mov ax, 0x9000 
@@ -51,7 +61,6 @@ mov cl, 2             ; sector
 mov dh, 0             ; disk head
 mov bx, 0x7e00        ; put kernel at 0x7e00
 int 0x13              ; reset disk controller
-jc error 
 
 cli
 lgdt [gdtr]                ; global descriptor table (register)
@@ -76,12 +85,14 @@ mov ss, eax
 ; set up aligned stack bottom
 mov esp, 0x7c00
 ; enable SSE instructions
-mov eax, cr4
-or eax, 512
-mov cr4, eax
-mov dword[gs:0x30], 0
-
-call main
+; mov eax, cr4
+; or eax, 512
+; mov cr4, eax
+; mov dword[gs:0x30], 
+mov esi, sss
+call sprint
+jmp $
+; call main
 
 gdtr:
     dw (gdt_end - gdt) + 1 ; size
@@ -112,6 +123,9 @@ gdt_end:
 
 msg db 'Hello World', 13, 10, 0
 err db 'Could not read disk', 13, 10, 0
+sss db 'Hi from 32 bits', 0
+xpos db 0
+ypos db 0
 
 times 510-($-$$) db 0      ; pad with 0s
 db 0x55                    ; place 0xAA55 at offset 510
