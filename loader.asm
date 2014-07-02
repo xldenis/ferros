da5d7d9050f0d669df5c5200ee507d67873314c6
Xavier Denis <xldenis@gmail.com>
2014-07-02 18:13:58 -0400

    Fix clearing the screen (aka 32 bit printing)

--- loader.asm
+++ loader.asm
@@ -10,36 +10,6 @@ org 0x7c00                 ; bootloader loaded to offset 0x7c00
 
 jmp start
 
-dochar:
-    call cprint              ; print one character
-sprint:
-    mov eax, [esi]          ; string char to AL
-    lea esi, [esi+1]
-    cmp al, 0
-    jne dochar               ; else, we're done
-    add byte [ypos], 1       ; down one row
-    mov byte [xpos], 0       ; back to left
-    ret
- 
-cprint:
-    mov ah, 0x0F             ; attrib = white on black
-    mov ecx, eax             ; save char/attribute
-    movzx eax, byte [ypos]
-    mov edx, 160             ; 2 bytes (char/attrib)
-    mul edx                  ; for 80 columns
-    movzx ebx, byte [xpos]
-    shl ebx, 1               ; times 2 to skip attrib
- 
-    mov edi, 0xb8000         ; start of video memory
-    add edi, eax             ; add y offset
-    add edi, ebx             ; add x offset
- 
-    mov eax, ecx             ; restore char/attribute
-    mov word [ds:edi], ax
-    add byte [xpos], 1       ; advance to right
- 
-    ret
-
 start: 
 xor ax, ax
 mov ds, ax
@@ -83,15 +53,18 @@ mov fs, eax
 mov gs, eax
 mov ss, eax
 ; set up aligned stack bottom
-mov esp, 0x7c00
+mov eax, 0x7bff
+mov esp, eax
 ; enable SSE instructions
 ; mov eax, cr4
 ; or eax, 512
 ; mov cr4, eax
 ; mov dword[gs:0x30], 
-mov esi, sss
-call sprint
-jmp $
+mov edi, 0xb8000
+mov ecx, 80*25*2
+mov al, 0
+rep stosb
+hlt
 ; call main
 
 gdtr:
